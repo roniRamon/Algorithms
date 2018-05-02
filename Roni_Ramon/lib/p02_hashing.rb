a933c4ccf2df3ca778685f6764e9ccfec8ef530f
RoniRamon <roniramon260@gmail.com>
2018-05-01 18:34:54 -0700

    finish p2

--- Roni_Ramon/lib/p02_hashing.rb
+++ Roni_Ramon/lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    self.join.to_i.hash
   end
 end
 
 class String
   def hash
+    arr = []
+    self.each_byte do |char|
+      arr.push(char)
+    end
+    arr.hash
   end
 end
 
@@ -16,6 +22,26 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    arr1 = self.keys
+    arr2 = self.values
+    all_items = []
+
+    arr1.each do |el|
+      if el.is_a? Numeric
+        all_items.push(el)
+      else
+        el = el.to_s
+        all_items.concat(el.bytes)
+      end
+    end
+    arr2.each do |el|
+      if el.is_a? Numeric
+        all_items.push(el)
+      elsif
+        el = el.to_s
+        all_items.concat(el.bytes)
+      end
+    end
+    all_items.sort.hash
   end
 end
