09e7ff785d4a4ed76d112c303ccaf128964131b3
RoniRamon <roniramon260@gmail.com>
2018-05-02 09:54:49 -0700

    finish p07

--- Roni_Ramon/lib/p07_practical.rb
+++ Roni_Ramon/lib/p07_practical.rb
@@ -1,5 +1,22 @@
 require_relative 'p05_hash_map'
+require 'byebug'
+def can_string_be_palindrome?(string)
+  hash_map = HashMap.new(string.length)
 
-def can_string_be_palindrome?(string) 
-  
+  string.chars do |char|
+    if hash_map.include?(char)
+       hash_map[char] = hash_map[char] + 1
+    else
+      hash_map.set(char, 1)
+    end
+  end
+
+  counter = 0
+  hash_map.each do |bucket|
+    if bucket[1].odd?
+      counter += 1
+    end
+  end
+  return false if counter > 1
+  true  
 end
