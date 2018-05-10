87580b7e01ec30accf7f6b0a422b330ff405054d
RoniRamon <roniramon260@gmail.com>
2018-05-09 18:51:30 -0700

    finished super frog hops

--- Roni_Ramon_project7/lib/dynamic_programming.rb
+++ Roni_Ramon_project7/lib/dynamic_programming.rb
@@ -16,9 +16,9 @@ class DynamicProgramming
   def blair_nums(n)
     return @blair_cache[n] unless @blair_cache[n].nil?
 
-    ans = blair_nums(n - 1) + blair_nums(n - 2) + ((2 * (n - 2)) + 1)
-    @blair_cache[n] = ans
-    return ans
+    result = blair_nums(n - 1) + blair_nums(n - 2) + ((2 * (n - 2)) + 1)
+    @blair_cache[n] = result
+    return result
   end
 
   def frog_hops_bottom_up(n) # 3
@@ -38,17 +38,46 @@ class DynamicProgramming
   end
 
   def frog_hops_top_down(n)
+    frog_hops_top_down_helper(n)
 
+    @frog_cache[n]
   end
 
   def frog_hops_top_down_helper(n)
-
+    return if n == 1
+    frog_hops_top_down_helper(n - 1)
+    if n > 3
+      ans =  @frog_cache[n - 3].map { |el| el + [3] }
+      ans2 = @frog_cache[n - 2].map { |el| el + [2] }
+      ans3 = @frog_cache[n - 1].map { |el| el + [1] }
+      @frog_cache[n] = ans + ans2 + ans3
+    end
+    @frog_cache[n]
   end
 
   def super_frog_hops(n, k)
+    #base case
+    super_cache = [ [[]], [[1]] ]
+    # return super_cache[n] if n <= 1
+    (super_cache.length..n).each do |j|
+      arr = []
+      counter = super_cache.length >= k ? super_cache.length - k : 0
+      (counter..super_cache.length-1).each do |i|
+        diff = (i - j).abs
+        if diff <= k
+          arr += super_cache[i].map { |steps| steps + [diff]}
+        end
+      end
+      super_cache.push(arr)
+    end
 
+    super_cache[n]
   end
 
+  # input:  weight =  [1, 2, 3]
+  #         values = [10, 4, 8]
+  #         capacity = 3
+  # output: 10 + 4 = 14
   def knapsack(weights, values, capacity)
 
   end
